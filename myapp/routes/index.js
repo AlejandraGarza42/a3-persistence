316fbfa454835bd431457f087ab31a9ef1c3ec3d
AlejandraGarza42 <agarza@wpi.edu>
2019-09-12 00:01:54 -0400

    started making authenticator
    Signed-off-by: AlejandraGarza42 <agarza@wpi.edu>

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,15 @@
+/* Original, dont want to delete just yet
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Website' });
+/* GET home page.
+router.get('/', function(req, res, next) { 
+  res.render('index', { title: 'Ale Website' });
 });
+
+module.exports = router;
+*/
+var express = require('./users');
+var router = express.Router();
 
 module.exports = router;
